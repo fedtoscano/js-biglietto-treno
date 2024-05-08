7e09e81058801313f3f4b728c6769702f6928621
fedtoscano <federicotoscano611@gmail.com>
2024-05-08 14:49:25 +0200

    corrections 2

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ if(userAge<18){
     let discount = ((price / 100) * 21).toFixed(2);
     let finalPrice = (price - discount).toFixed(2);
 
-    console.log(`Vuoi percorrere ${userKm}km. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
+    console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${price}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
 } else if(userAge>65){
         let discount = ((price / 100) * 42).toFixed(2);
         let finalPrice = (price - discount).toFixed(2);
 
-        console.log(`Vuoi percorrere ${userKm}km. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
+        console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${price}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
         } else {
 
             console.log(`Vuoi percorrere ${userKm}km. Non hai diritto ad alcuno sconto, quindi il prezzo totale è di ${price}€`);
