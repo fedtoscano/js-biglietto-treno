f31098312d73fa817e0a5e028b7321d3fee3bd4b
fedtoscano <federicotoscano611@gmail.com>
2024-05-09 10:35:25 +0200

    changed the name of price to initialPrice

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 let userKm = Number.parseInt(prompt("Inserisci il numero di KM che vuoi percorrere"), 10)
 let userAge = Number.parseInt(prompt("Inserisci la tua età"), 10)
 
-let price = userKm * 0.276
+let initialPrice = userKm * 0.276
 
 if(userAge<18){
-    let discount = ((price / 100) * 21).toFixed(2);
-    let finalPrice = (price - discount).toFixed(2);
-
-    console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${price}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
+    let discount = ((initialPrice / 100) * 21).toFixed(2);
+    let finalPrice = (initialPrice - discount).toFixed(2);
+    console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${initialPrice}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
 } else if(userAge>65){
-        let discount = ((price / 100) * 42).toFixed(2);
-        let finalPrice = (price - discount).toFixed(2);
-
-        console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${price}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
+        let discount = ((initialPrice / 100) * 42).toFixed(2);
+        let finalPrice = (initialPrice - discount).toFixed(2);
+        console.log(`Vuoi percorrere ${userKm}km. Il prezzo di listino è ${initialPrice}€. Il tuo sconto è ${discount}€, quindi il prezzo totale è di ${finalPrice}€`);
         } else {
-
-            console.log(`Vuoi percorrere ${userKm}km. Non hai diritto ad alcuno sconto, quindi il prezzo totale è di ${price}€`);
-}
+            console.log(`Vuoi percorrere ${userKm}km. Non hai diritto ad alcuno sconto, quindi il prezzo totale è di ${initialPrice}€`);
+        }
 
